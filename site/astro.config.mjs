5e611f6bf8c22bd1b995a3d23e1ecd313d73cb4e
TheLazySquid <thelazysquid2@gmail.com>
2025-05-08 21:43:37 -0400

    Who knows, maybe this works

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,15 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import svelte from '@astrojs/svelte';
-
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from "path";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: [
+        {
+          find: "normalize-js-code",
+          replacement: resolve("..", "lib")
+        }
+      ]
+    }
   }
 });
